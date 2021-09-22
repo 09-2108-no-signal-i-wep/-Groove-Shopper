508fcf753113d1b962067525be2d44c0041fc443
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-22 18:45:07 -0400

    All albums now properly render, added some CSS to make page fully visable.

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,14 @@
-const router = require('express').Router()
-module.exports = router
+const router = require('express').Router();
+const usersRouter = require('./users');
+const albumsRouter = require('./albums');
 
-router.use('/users', require('./users'))
+router.use('/users', usersRouter);
+router.use('/albums', albumsRouter);
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+module.exports = router;
