35d1051ce11e9f48401098bacd4d8f86f3be6f80
Ed <econde@technicacorp.com>
2021-09-23 16:27:55 -0500

    C:/Program Files/Git/server/api/cart.js init & adding server seeds

--- server/db/index.js
+++ server/db/index.js
@@ -12,14 +12,44 @@ const OrderAlbum = require('./models/OrderAlbum');
 User.hasMany(Order);
 Order.belongsTo(User);
 
-Album.belongsToMany(Order, { through: OrderAlbum });
-Order.belongsToMany(Album, { through: OrderAlbum });
+Album.belongsToMany(Order, {
+  through: OrderAlbum, as: 'orders'
+});
+Order.belongsToMany(Album, {
+  through: OrderAlbum, as: 'products'
+});
 
 Album.hasOne(Artist);
 Artist.belongsTo(Album);
 
 Album.hasOne(Genre);
 Genre.belongsTo(Album);
+
+
+
+const showMagicMethods = async () => {
+  const order = await Order.create({
+    userId: 1,
+    isCart: true,
+    total: 5000,
+  });
+
+const album = await Album.create({
+  title: "Flying Microtonal Banana",
+  artistId: 1,
+  price: 10.99,
+  releaseYear: 2007,
+  cover: "https://f4.bcbits.com/img/a2731568276_10.jpg",
+})
+console.log('AHHHH', order, album
+)
+  await order.addAlbum(album)
+
+  console.log('TEST ORDER', order)
+  //console.log('METHODS', order.__proto__)
+}
+
+//showMagicMethods();
 
 module.exports = {
   db,
