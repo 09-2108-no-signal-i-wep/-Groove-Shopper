0e2bec37f7bab587eb2909291ba900f8294dace2
Greg Schotte <schotte.greg@gmail.com>
2021-09-23 14:43:51 -0400

    added singleAlbumView page and add-to-cart button

--- client/components/SingleAlbum.js
+++ client/components/SingleAlbum.js
@@ -7,29 +7,54 @@ import { fetchSingleAlbum } from "../redux/singleAlbum";
 class SingleAlbum extends Component {
   constructor() {
     super();
+    this.state = {
+      loading: true,
+    };
+    this.adjustPrice = this.adjustPrice.bind(this);
   }
 
   componentDidMount() {
+    console.log("*** COMPONENT MOUNTING *****");
     const albumId = this.props.match.params.albumId;
+    this.setState({
+      loading: false,
+    });
     this.props.fetchSingleAlbum(albumId);
+    console.log("*** COMPONENT MOUNTED *****");
+  }
+
+  adjustPrice(price) {
+    return price / 100;
   }
 
   render() {
-    console.log('proppy', this.props);
-    const {cover, price, releaseYear, title, artist} = this.props.singleAlbum;
-    return (
-      <div className="single-album-container">
-        <div id = 'album-artwork-container'>
-          <img src={cover} className='single-album-artwork'/>
-          <h5>Released: {releaseYear}</h5>
-        </div>
-        <div className='single-album-info'>
-          <h3>{title}</h3>
-          <h4>{price}</h4>
+    //console.log("artist", this.props.singleAlbum.artist.name);
+    console.log("props.singleAlbum", this.props.singleAlbum);
+    const { cover, price, releaseYear, title } = this.props.singleAlbum;
+    const artist = this.props.singleAlbum.artist;
+    console.log("ARTIST", artist);
+    const { loading } = this.state;
+    // console.log('load', loading)
+    const { adjustPrice } = this;
 
+    if (loading) {
+      return <h1>LOADING</h1>;
+    } else {
+      return (
+        <div className="single-album-container">
+          <div id="album-artwork-container">
+            <img src={cover} className="single-album-artwork" />
+            <h5>Released: {releaseYear}</h5>
+          </div>
+          <div className="single-album-info">
+            <h2>{title}</h2>
+            {/* <h2>{artist.name}</h2> */}
+            <h3>{`$ ${adjustPrice(price)}`}</h3>
+            <button type='submit' id='add-to-cart'>Add To Cart!</button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
